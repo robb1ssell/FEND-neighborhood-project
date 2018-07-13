9669dceeb09e19dc9098794b7a7d571b53910d2e
Robert Bissell <roblincolnbissell@gmail.com>
2018-07-13 10:53:20 -0500

    add markers.js, add libs think i might need

---
+++ src/components/Markers.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import escapeRegExp from 'escape-string-regexp'
+import scriptLoader from 'react-async-script-loader'
+import fetchJsonp from 'fetch-jsonp'
+
+let markers = []
+let infoWindows = []
+
+class Markers extends React.Component {
+    
+    
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Markers
