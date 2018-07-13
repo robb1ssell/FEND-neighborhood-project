a02a1bb610339e7053bc7cea79f76211a1e00bf9
Robert Bissell <roblincolnbissell@gmail.com>
2018-07-13 11:05:00 -0500

    add input for search

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -7,12 +7,30 @@ let markers = []
 let infoWindows = []
 
 class Markers extends React.Component {
-    
+    constructor(props) {
+        super(props) // bring props from parent to access locations array
+        this.state = {
+            list: this.props.locations,
+            map: {}, // holds our map object
+            info: [],
+            query: '' // used to isolate markers during search
+        }
+    }
     
     render() {
         return(
-            <div>
-
+            <div id="marker-list">
+                <div id="marker-search">
+                    <input
+                        className="search-markers"
+                        type="text"
+                        role="search"
+                        aria-label="Type to search markers on the map"
+                        placeholder="Search Houston"
+                        value={query}
+                        onChange={something}
+                    />
+                </div>
             </div>
         )
     }
