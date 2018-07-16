52b20ac62208344c24bdcc6815e0a909ae5b3482
Robert Bissell <roblincolnbissell@gmail.com>
2018-07-16 15:03:29 -0500

    moved locations to separate file. added import in app.js

---
+++ src/data/locations.js
@@ -0,0 +1,11 @@
+export const locations = [
+        { name: 'Houston Zoo', lat: 29.714234, lng: -95.388654},
+        { name: 'Houston Arboretum', lat: 29.765215, lng: -95.451973},
+        { name: 'NRG Stadium', lat: 29.684703, lng: -95.410921},
+        { name: 'Hobby Airport', lat: 29.645592, lng: -95.277473},
+        { name: 'Downtown Aquarium', lat: 29.764336, lng: -95.367355},
+        { name: 'Minute Maid Park', lat: 29.757319, lng: -95.355272},
+        { name: 'Sam Houston Race Park', lat: 29.929746, lng: -95.530073},
+        { name: 'George Bush Intercontinental Airport', lat: 29.987638, lng: -95.336989},
+        { name: 'Sam Houston Statue', lat: 30.661781, lng: -95.510702}
+    ];
